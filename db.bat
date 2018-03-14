5e9158f7ddc469dd91be1b156fd033fe2c38c471
ivan deng <guohuadeng@hotmail.com>
2018-03-15 01:20:07 +0800

    update

--- db.bat
+++ db.bat
@@ -1,10 +1,13 @@
 title GreenOdoo11 x64 fast - www.sunpop.cn
-SET PATH=%CD%\runtime\pgsql\bin;%CD%\runtime\python;%CD%\runtime\python\scripts;%CD%\runtime\win32\wkhtmltopdf;%CD%\runtime\win32\nodejs;
+%CD%\runtime\bin\pv.exe -f -k python.exe -q
+%CD%\runtime\bin\pv.exe -f -k python3.exe -q
 %CD%\runtime\pgsql\bin\pg_ctl stop -D %CD%\runtime\pgsql\data -s -m fast
 %CD%\runtime\bin\pv.exe -f -k postgres.exe -q
 tskill postgres
 tskill python
+tskill python3
 tskill nginx
-ping -n 3 127.0.0.1>nul
-cd .\runtime\nginx & START nginx.exe &cd.. &cd..
+ping -n 2 127.0.0.1>nul
+SET PATH=%CD%\runtime\pgsql\bin;%CD%\runtime\python3;%CD%\runtime\python3\scripts;%CD%\runtime\win32\wkhtmltopdf;%CD%\runtime\win32\nodejs;%CD%\source;%PATH%
+cd runtime\nginx &START nginx.exe &cd.. &cd..
 %CD%\runtime\pgsql\bin\pg_ctl -D %CD%\runtime\pgsql\data -l %CD%\runtime\pgsql\logfile start
